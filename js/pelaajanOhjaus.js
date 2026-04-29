32214b8c637859a77257539abce9d6f86bc9ea4c
Jedinakki <167286152+Jedinakki@users.noreply.github.com>
2026-04-29 11:53:07 +0300

    Update pelaajanOhjaus.js

--- js/pelaajanOhjaus.js
+++ js/pelaajanOhjaus.js
@@ -1,27 +1,13 @@
-//const canvas = document.getElementById('gameCanvas');
-//const ctx = canvas.getContext('2d');
-
-// let x = 50;
-// let y = 50;
-
-// const pLeveys = 30
-// const pPituus = 30
-
-// const paino = 4
-// let maassa = false
-
-// const nopeus = 5;
-
-    // Muutettu jotta olisi yhteensopiva muun koodin kanssa -Roni
 
 const player = {
     x: 50,
     y: 50,
     pLeveys: 30,
     pPituus: 30,
-    paino: 4,
+    paino: 6,
     maassa: false,
     nopeus: 5,
+    hyppy: 10,
     hyppays: false,
     korkeusTarkastus: 0
 };
@@ -33,13 +19,6 @@ vasemmalle = false
 let hyppays = false
 let korkeusTarkastus = 0
 
-// Siirretty js/script.js
-
-//function drawPlayer() {
-    //ctx.clearRect(0, 0, canvas.width, canvas.height);  
-    //ctx.fillStyle = 'rgb(90, 178, 230)';
-    //ctx.fillRect(x, y, pLeveys, pLeveys);  
-//}
 
 document.addEventListener('keydown', liikutaPelaajaa);
 document.addEventListener("keyup",lopetaLiikutus)
@@ -76,7 +55,7 @@ switch(e.key) {
 //uusi liikkumis ohjaus
 function liikutus(){
     if (hyppataan === true&&player.maassa===true||player.hyppays === true){
-        player.y -= nopeus 
+        player.y -= player.hyppy 
         player.korkeusTarkastus += 1
         player.hyppays = true
         if (player.korkeusTarkastus === 15){
@@ -92,15 +71,7 @@ function liikutus(){
 }
 
 
-//piirtää peli alueen
-//function piirräMaa(){
-    //ctx.fillStyle = "black"
-    //ctx.lineWidth = 4
-    //ctx.beginPath()
-    //ctx.moveTo(0,400)
-    //ctx.lineTo(800,400)
-    //ctx.stroke()
-//}
+
 
 //asettaa putoamis nopeuden kun on ilmassa
 function painovoima(){
@@ -109,19 +80,4 @@ function painovoima(){
     }
 }
 
-//yksinkertainen colllison checki testausta varten
-function checkCollision() {
-    if (y + 35 > 400) {
-        maassa = true
-    } else {
-        maassa = false
-    }
-}
 
-//setInterval(function() {
-    //checkCollision()
-    //liikutus()
-    //painovoima()
-    //drawPlayer();
-    //piirräMaa()
-//}, 20);
